1213d1e50b44055557fbfd94c554ce240d60056a
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-11-21 17:29:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n, a=1, b=1,c;
+    cin >> n;
+    if (n<2){
+        cout << "errore";
+    }
+    else {
+        for(int i=1; i<=n; i++){
+            cout <<a<< endl;
+            b=c;
+            c=a;
+            a=b+c;
+        }
+    }
+    return 0;
 }
