f8452d08747ca8c1fa8350ae10e9ec83a67dc042
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-11-21 17:42:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ int main() {
     int n, a=1, b=1,c;
     cin >> n;
     if (n>=2){
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<n; i++){
             cout <<a<< endl;
             b=c;
             c=a;
             a=b+c;
         }
     }
-    else {        
+    else {
         cout << "errore";
     }
     return 0;
