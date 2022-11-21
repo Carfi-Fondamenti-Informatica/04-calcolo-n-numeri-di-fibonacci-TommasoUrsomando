bfe2257dfa74d18007b3144605f0f3939f17f118
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-11-21 17:37:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
         cout << "errore";
     }
     else {
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<n; i++){
             cout <<a<< endl;
             c=a;
             a=b;
